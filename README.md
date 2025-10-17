77ad3e78ba327dfbd9c7d8f8e78bf7faf587fb6d
AlIsa-3 <182702391+AlIsa-3@users.noreply.github.com>
2025-10-17 15:44:53 +0100

    Add java implementation. Able to run higher number of simulations in shorter time, however outcome is highly variable at low simulation count. Prefer Python implementation for stability.

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # Genshin Impact Wish Probability Monte-Carlo Simulation
 
-A Python script implementing a Monte-Carlo simulation to compute the approximate probabilities of getting $n$ limited 5-stars in $N$ wishes.
+ Monte-Carlo simulation to compute the approximate probabilities of getting $n$ limited 5-stars in $N$ wishes, implemented in Python and Java. 
 
+
+The Python implementation is more stable, however it runs large numbers of simulations slowly. The Java implementation can run large numbers of simulations faster, but has high variability in its outcomes at low numbers.
 
 ## Usage:
+
+### Python:
 
 Supply arguments through the console
 
@@ -11,3 +15,12 @@ Usage examples:
 To simulate $300$ wishes with $1000$ times without guarantee on a banner with hard-pity $= 90$ and no starting pity with a target of $3$ limited 5-stars:
 
 ```python3 simulation.py --simulation-count 1000 300 3 0 90```
+
+### Java
+
+Download the `Simulation.java` file and place it into a directory. From that directory, run:
+
+```javac Simulation.java```
+and then run 
+```java Simulation``` 
+and follow the prompts on the console
---
+++ Simulation.java
@@ -0,0 +1,173 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Simulation {
+
+
+    // Define Simulation Parameters
+    public int wish_count;
+    public int target_5_star_count;
+    public int current_pity;
+    public int banner_pity;
+    public int simulation_count;
+    public int CR_score;
+    public boolean isGuaranteed;
+    
+
+    private boolean soft_pity(){
+
+        int soft_pity_value = banner_pity - current_pity;
+        Random rng = new Random(System.currentTimeMillis());
+        
+        int true_weight = 6 * (1 + 16 - soft_pity_value);
+
+        if (soft_pity_value <= 16){
+            // Weighted Random
+            int soft_pity_int = rng.nextInt(101);
+            return soft_pity_int <= true_weight;
+        }
+        else{
+            // If the selected integer is a specific value --> got a 5-star
+            int chosen_int = rng.nextInt(1000);
+            return chosen_int == 6;
+        }
+    }
+
+    private boolean capturing_radiance(){
+        if (CR_score == 3){
+            CR_score = 1;
+            return true;
+        }
+        else{
+            CR_score += 1;
+            return false;
+        }
+    }
+
+    private boolean fifty_fifty(){
+        Random rng = new Random(System.currentTimeMillis());
+        int num = rng.nextInt(2);
+
+        boolean choice = (num == 1);
+
+        if (choice == false){
+            // Try capturing radiance
+            choice = capturing_radiance();
+        }
+
+        return choice;
+    }
+
+
+    private boolean five_star(){
+        boolean isLimited;
+
+        // Reset Pity
+        current_pity = 0;
+
+        if (isGuaranteed){
+            
+            isGuaranteed = false;
+            isLimited = true;
+        }
+        else{
+            isLimited = fifty_fifty();
+            isGuaranteed = !isLimited;
+        }
+        return isLimited;
+    }
+
+
+    private boolean one_wish(){
+        boolean isLimited = false;
+
+        // Check if reached pity or soft pity
+        if (current_pity == banner_pity | soft_pity()){
+            return five_star();
+        }
+        else{
+            current_pity += 1;
+            return isLimited;
+        }
+
+    }
+
+    private boolean one_run(){
+        int limited_count = 0;
+        for (int i = 0; i < wish_count; i++){
+
+            limited_count += (one_wish()) ? 1 : 0;
+
+        }
+        return limited_count >= target_5_star_count;
+    }
+
+
+    private double simulation(){
+
+        int original_CR_score = CR_score;
+        int original_current_pity = current_pity;
+        boolean original_isGuaranteed = isGuaranteed;
+        
+        double successes = 0;
+
+        for (int i = 0; i<simulation_count; i++){
+
+            successes += (one_run()) ? 1 : 0;
+
+            CR_score = original_CR_score;
+            current_pity = original_current_pity;
+            isGuaranteed = original_isGuaranteed;
+
+        }
+        return successes / simulation_count;
+    }
+
+
+    private void display_results(double probability){
+
+        String outputString = "The probability of getting %d limited 5-stars in %d wishes is approximately %.2f%% (Calculated using %d simulations)";
+
+        outputString = String.format(outputString,target_5_star_count,wish_count,probability*100,simulation_count);
+
+        System.out.println(outputString);
+
+    }
+
+    private Simulation(){
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.print("Enter the number of wishes: ");
+            wish_count = Integer.parseInt(input.nextLine());
+            
+            System.out.print("Enter the target number of limited 5-stars: ");
+            target_5_star_count = Integer.parseInt(input.nextLine());
+            
+            System.out.print("Enter the current pity: ");
+            current_pity = Integer.parseInt(input.nextLine());
+            
+            System.out.print("Enter the banner pity: ");
+            banner_pity = Integer.parseInt(input.nextLine());
+            
+            System.out.print("Is it Guaranteed, Y/N: ");
+            isGuaranteed = ("Y".equals(input.nextLine().toUpperCase()));
+            
+            
+            System.out.print("How many simulations: ");
+            simulation_count = Integer.parseInt(input.nextLine());
+        }
+    }
+
+    private void run(){
+
+        double probability = simulation();
+        display_results(probability);
+
+
+        
+    }
+    
+    public static void main(String[] args) {
+        Simulation sim = new Simulation();
+        sim.run();
+    }
+}
